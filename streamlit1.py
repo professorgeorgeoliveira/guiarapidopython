fafe5f924de06bcbcde270e501f5a36e99cc574c
George Oliveira <39756648+professorgeorgeoliveira@users.noreply.github.com>
2026-06-20 19:36:45 -0400

    Initialize Streamlit app with basic components
    Add basic Streamlit app with title, headers, and text.

--- streamlit1.py
+++ streamlit1.py
@@ -1,1 +1,11 @@
+import streamlit as st
 
+# Exibindo textos com diferentes hierarquias
+st.title("Meu Primeiro App 🚀")
+st.header("Este é um cabeçalho")
+st.subheader("Este é um sub-cabeçalho")
+st.text("Este é um texto simples, sem formatação Markdown.")
+
+# Usando o st.write para formatação flexível
+ferramenta = "Streamlit"
+st.write(f"Aprendendo a criar interfaces com o **{ferramenta}**!")
